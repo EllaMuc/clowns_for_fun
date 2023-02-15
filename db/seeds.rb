699340f6e608510d6d9a232a5a48bb8d04652469
Tom Elsom <te28@hotmail.com>
2023-02-15 20:42:17 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts "destroy users and clowns"
+
+Clown.destroy_all
+User.destroy_all
+
+puts "creates two users"
+
+user1 = User.create(email: "user1@gmail.com", password: "123456")
+user2 = User.create(email: "user2@gmail.com", password: "123456")
+
+puts "create 5 clowns"
+
+5.times do
+  clown = Clown.create(name: Faker::FunnyName.name, user_id: user1.id)
+  puts "#{clown.name}"
+end
