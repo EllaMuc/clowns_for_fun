cc899670b8bb6113ae069b28772d9e2592043eb7
Tom Elsom <te28@hotmail.com>
2023-02-17 18:01:51 +0000

    changes to the controller in pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,22 @@ class PagesController < ApplicationController
   def index
     @clown = Clown.all
   end
+
   def create
     @clown = Clown.create
+  end
+
+  def update
+    if @clown.update(clown_params)
+      redirect_to @clown
+    else
+    render :edit, status: :unprocessable_entity
+
+    end
+  end
+
+  def destroy
+    @clown.destroy
+  redirect_to clown_url
   end
 end
