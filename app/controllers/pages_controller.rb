bb2f82e4198cdbb3db90033a015d599da90d7b04
Tom Elsom <te28@hotmail.com>
2023-02-18 11:33:17 +0000

    humepage update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,21 +3,4 @@ class PagesController < ApplicationController
     @clowns = Clown.all
   end
 
-  def create
-    @clown = Clown.create
-  end
-
-  def update
-    if @clown.update(clown_params)
-      redirect_to @clown
-    else
-    render :edit, status: :unprocessable_entity
-
-    end
-  end
-
-  def destroy
-    @clown.destroy
-  redirect_to clown_url
-  end
 end
