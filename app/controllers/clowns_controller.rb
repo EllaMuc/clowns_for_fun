31f74bf0d340f62525a0a5fd4fe400862c4cad52
Hugo Voorheijen <h.voorheijen@gmail.com>
2023-02-18 12:04:23 +0100

    editclown

--- app/controllers/clowns_controller.rb
+++ app/controllers/clowns_controller.rb
@@ -4,9 +4,27 @@ class ClownsController < ApplicationController
   def show
   end
 
+  def edit
+  end 
+
+  def update
+    if @clown.update(clown_params)
+      redirect_to @clown
+    else
+      render :edit
+    end
+  end
+
+
+
+
   private
 
   def set_clown
     @clown = Clown.find(params[:id])
+  end
+
+  def clown_params
+    params.require.(:clown).permit(:name, :address, :price, :description)
   end
 end
