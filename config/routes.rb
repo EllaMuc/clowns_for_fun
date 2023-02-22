251731812756ba5c896a090472ef1c421ebfa93a
Michaela Katzenmueller <m.katzenmueller@gmx.de>
2023-02-22 16:43:57 +0100

    Reservation

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :clowns, except: [:index]
+  # resources :clowns, except: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :reservations, only: [:create]
+  resources :clowns, except: [:index] do
+    resources :reservations, only: [:create]
+  end
 end
