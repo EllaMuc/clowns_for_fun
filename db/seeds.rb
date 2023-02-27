a58e923250b8842e29a2299b9b108e21c678f943
Tom Elsom <te28@hotmail.com>
2023-02-27 13:01:24 +0000

    new buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,13 @@ puts "creates two users"
 user1 = User.create(email: "user1@gmail.com", password: "123456")
 user2 = User.create(email: "user2@gmail.com", password: "123456")
 
-puts "create 5 clowns"
+puts "create 20 clowns"
 
 20.times do
   clown = Clown.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph_by_chars(number: 150, supplemental: false), user_id: user1.id)
-  file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
-  clown.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+  file = URI.open('https://thispersondoesnotexist.com/image')
+  clown.photo.attach(io: file, filename: "user.png", content_type: "image/png")
+
   clown.save
-  puts "#{clown.name} #{clown.description}"
+  puts "#{clown.image} #{clown.name} #{clown.description}"
 end
